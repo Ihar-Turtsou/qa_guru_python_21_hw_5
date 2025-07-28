46c66272dbb646139bcf42030a2832282772fd9e
Ihar-Turtsou <ig.turtsov@gmail.com>
2025-07-29 00:31:20 +0400

    add test_form_submission test

--- tests/test_form_submission.py
+++ tests/test_form_submission.py
@@ -1,6 +1,49 @@
-from selene import browser,have
+import os
+from selene import browser, have, be
 
 def test_form_submission(setup_browser):
-    #browser.element('[id="firstName"]').type('Carl')
-    #browser.element('[id="lastName"]').type('Johnson')
+    browser.open('/')
+    browser.execute_script('document.querySelector("iframe").remove()')
+    browser.element('[id="firstName"]').type('Carla')
+    browser.element('[id="lastName"]').type('Johnson')
+    browser.element('[id="userEmail"]').type('Johnson@gmail.pom')
+    browser.element('//input[@name="gender" and @value="Female"]/following-sibling::label').click()
+    browser.element('[id="userNumber"]').type('7788995511')
+
+    browser.element('[id = "dateOfBirthInput"]').click()
+    browser.all('.react-datepicker__month-select option').element_by(have.exact_text('August')).click()
+    browser.all('.react-datepicker__year-select option').element_by(have.exact_text('1995')).click()
+    browser.element('[aria-label="Choose Saturday, August 5th, 1995"]').should(be.visible).click()
+
+    browser.element('[id="subjectsInput"]').type('Maths').press_enter()
+    browser.element('#subjectsInput').type('p')
+    browser.all('.subjects-auto-complete__menu div').element_by(have.exact_text('Physics')).click()
+
+    browser.element('//*[@id="hobbiesWrapper"]//label[contains(text(),"Reading")]').click()
+
+    browser.element('[id="uploadPicture"]').send_keys(os.path.abspath('resources/Screenshot_2368.png'))
+
+    browser.element('[id="currentAddress"]').set_value('South Street, PA, Philadelphia, 19147')
+
+    browser.element('#state input').type('Haryana').press_enter()
+    browser.element('#city input').type('Panipat').press_enter()
+
+    browser.element('[id="submit"]').click()
+
+    browser.element('.table-responsive').all('tr').should(
+        have.exact_texts(
+            'Label Values',
+            'Student Name Carla Johnson',
+            'Student Email Johnson@gmail.pom',
+            'Gender Female',
+            'Mobile 7788995511',
+            'Date of Birth 05 August,1995',
+            'Subjects Maths, Physics',
+            'Hobbies Reading',
+            'Picture Screenshot_2368.png',
+            'Address South Street, PA, Philadelphia, 19147',
+            'State and City Haryana Panipat',
+        )
+    )
     print('test finished')
+    # breakpoint()
