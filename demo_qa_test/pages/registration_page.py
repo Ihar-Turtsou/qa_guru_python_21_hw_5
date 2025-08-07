89e01e27a115dc982279573e9e7ab46f51d6b12d
Ihar-Turtsou <ig.turtsov@gmail.com>
2025-08-08 00:04:07 +0400

    add second part of refactoring

---
+++ demo_qa_test/pages/registration_page.py
@@ -0,0 +1,82 @@
+from selene import browser, have, be, by
+from demo_qa_test import resource
+
+
+class RegistrationPage:
+    def open(self):
+        browser.open('/automation-practice-form')
+        browser.execute_script('window.scrollBy(0, 500)')
+        return self
+
+    def fill_first_name(self, value):
+        browser.element('[id="firstName"]').type(value)
+        return self
+
+    def fill_last_name(self, value):
+        browser.element('[id="lastName"]').type(value)
+        return self
+
+    def fill_birthday(self, month, year, day):
+        browser.element('[id = "dateOfBirthInput"]').click()
+        browser.all('.react-datepicker__month-select option').element_by(have.exact_text(month)).click()
+        browser.all('.react-datepicker__year-select option').element_by(have.exact_text(year)).click()
+        browser.element(f'[aria-label="Choose Saturday, August {day}th, 1995"]').should(be.visible).click()
+        return self
+
+    def fill_email(self, email):
+        browser.element('[id="userEmail"]').type(email)
+        return self
+
+    def set_gender(self, value):
+        browser.element('#genterWrapper').element(by.text(value)).click()
+        return self
+
+    def fill_phone_number(self, value):
+        browser.element('[id="userNumber"]').type(value)
+        return self
+
+    def set_subject_by_enter(self, value):
+        browser.element('[id="subjectsInput"]').type(value).press_enter()
+        return self
+
+    def set_subject_by_click(self, type_letter, value):
+        browser.element('#subjectsInput').type(type_letter)
+        browser.all('.subjects-auto-complete__menu div').element_by(have.exact_text(value)).click()
+        return self
+
+    def set_hobby(self, value):
+        browser.element('#hobbiesWrapper').element(by.text(value)).click()
+        return self
+
+    def upload_picture(self, value):
+        browser.element('[id="uploadPicture"]').send_keys(resource.path(value))
+        return self
+
+    def fill_current_address(self, value):
+        browser.element('[id="currentAddress"]').set_value(value)
+        return self
+
+    def choose_location(self, state, city):
+        browser.element('#state input').type(state).press_enter()
+        browser.element('#city input').type(city).press_enter()
+        return self
+
+    def submit_form(self):
+        browser.element('[id="submit"]').click()
+        return self
+
+    def should_registred_user_with(self, full_name, email, gender, phone_number, birthday, first_subject,
+                                   second_subject, file_name, address, state_city):
+        browser.all('tbody tr td:nth-child(2)').should(have.exact_texts(
+            full_name,
+            email,
+            gender,
+            phone_number,
+            birthday,
+            first_subject,
+            second_subject,
+            file_name,
+            address,
+            state_city
+        ))
+        return self
--- tests/test_form_submission.py
+++ tests/test_form_submission.py
@@ -1,77 +1,38 @@
-from selene import browser, have, be
-from selene.core.query import value
-
-from demo_qa_test import resource
-
-
-class RegistrationPage:
-    def open(self):
-        browser.open('/automation-practice-form')
-
-    def fill_first_name(self, value):
-        browser.element('[id="firstName"]').type(value)
-
-    def fill_last_name(self, value):
-        browser.element('[id="lastName"]').type(value)
-
-    def fill_birthday(self, month, year, day):
-        browser.element('[id = "dateOfBirthInput"]').click()
-        browser.all('.react-datepicker__month-select option').element_by(have.exact_text(month)).click()
-        browser.all('.react-datepicker__year-select option').element_by(have.exact_text(year)).click()
-        browser.element(f'[aria-label="Choose Saturday, August {day}th, 1995"]').should(be.visible).click()
-
-    def fill_email(self, email):
-        browser.element('[id="userEmail"]').type(email)
+from demo_qa_test.pages.registration_page import RegistrationPage
 
 
 def test_form_submission(setup_browser):
     registration_page = RegistrationPage()
 
     registration_page.open()
-
-    registration_page.fill_first_name('Carla')
-    registration_page.fill_last_name('Johnson')
-    registration_page.fill_email('Johnson@gmail.pom')
-
-    #registration_page.set_gender()
-
-    browser.element('//input[@name="gender" and @value="Female"]/following-sibling::label').click()
-    browser.element('[id="userNumber"]').type('7788995511')
-
-    browser.execute_script('window.scrollBy(0, 500)')
-
-    registration_page.fill_birthday('August', '1995', '5')
-
-
-    browser.element('[id="subjectsInput"]').type('Maths').press_enter()
-    browser.element('#subjectsInput').type('p')
-    browser.all('.subjects-auto-complete__menu div').element_by(have.exact_text('Physics')).click()
-
-    browser.element('//*[@id="hobbiesWrapper"]//label[contains(text(),"Reading")]').click()
-
-    browser.element('[id="uploadPicture"]').send_keys(resource.path('Screenshot_2368.png'))
-
-    browser.element('[id="currentAddress"]').set_value('South Street, PA, Philadelphia, 19147')
-
-    browser.element('#state input').type('Haryana').press_enter()
-    browser.element('#city input').type('Panipat').press_enter()
-
-    browser.element('[id="submit"]').click()
-
-    browser.element('.table-responsive').all('tr').should(
-        have.exact_texts(
-            'Label Values',
-            'Student Name Carla Johnson',
-            'Student Email Johnson@gmail.pom',
-            'Gender Female',
-            'Mobile 7788995511',
-            'Date of Birth 05 August,1995',
-            'Subjects Maths, Physics',
-            'Hobbies Reading',
-            'Picture Screenshot_2368.png',
-            'Address South Street, PA, Philadelphia, 19147',
-            'State and City Haryana Panipat',
-        )
+    (
+    registration_page
+    .fill_first_name('Carla')
+    .fill_last_name('Johnson')
+    .fill_email('Johnson@gmail.pom')
+    .set_gender("Female")
+    .fill_phone_number('7788995511')
+    .fill_birthday('August', '1995', '5')
+    .set_subject_by_enter('Maths')
+    .set_subject_by_click('p', 'Physics')
+    .set_hobby("Reading")
+    .upload_picture('Screenshot_2368.png')
+    .fill_current_address('South Street PA Philadelphia 19147')
+    .choose_location('Haryana', 'Panipat')
+    .submit_form()
     )
+
+    registration_page.should_registred_user_with(
+            'Carla Johnson',
+            'Johnson@gmail.pom',
+            'Female',
+            '7788995511',
+            '05 August,1995',
+            'Maths, Physics',
+            'Reading',
+            'Screenshot_2368.png',
+            'South Street PA Philadelphia 19147',
+            'Haryana Panipat'
+                                                 )
     print('test finished')
     # breakpoint()
