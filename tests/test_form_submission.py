fee4413b6e77f472982b53c1c82b1372eb8f6867
Ihar-Turtsou <ig.turtsov@gmail.com>
2025-08-18 17:50:49 +0400

    fix selectors and add allure

--- tests/test_form_submission.py
+++ tests/test_form_submission.py
@@ -1,26 +1,25 @@
 from pathlib import Path
-from selene import browser, have, be
+from selene import browser, have, be, by
 
 def test_form_submission(setup_browser):
     browser.open('/automation-practice-form')
+    browser.execute_script('window.scrollBy(0, 550)')
     browser.element('[id="firstName"]').type('Carla')
     browser.element('[id="lastName"]').type('Johnson')
     browser.element('[id="userEmail"]').type('Johnson@gmail.pom')
-    browser.element('//input[@name="gender" and @value="Female"]/following-sibling::label').click()
+    browser.element('#genterWrapper').element(by.text('Female')).click()
     browser.element('[id="userNumber"]').type('7788995511')
-
-    browser.execute_script('window.scrollBy(0, 500)')
 
     browser.element('[id = "dateOfBirthInput"]').click()
     browser.all('.react-datepicker__month-select option').element_by(have.exact_text('August')).click()
     browser.all('.react-datepicker__year-select option').element_by(have.exact_text('1995')).click()
-    browser.element('[aria-label="Choose Saturday, August 5th, 1995"]').should(be.visible).click()
+    browser.all(f'.react-datepicker__day:not(.react-datepicker__day--outside-month)').element_by(have.exact_text(str(int('5')))).click()
 
     browser.element('[id="subjectsInput"]').type('Maths').press_enter()
     browser.element('#subjectsInput').type('p')
     browser.all('.subjects-auto-complete__menu div').element_by(have.exact_text('Physics')).click()
 
-    browser.element('//*[@id="hobbiesWrapper"]//label[contains(text(),"Reading")]').click()
+    browser.element('#hobbiesWrapper').element(by.text('Reading')).click()
 
     browser.element('[id="uploadPicture"]').set_value(
         str((Path(__file__).parent / 'resources' / 'Screenshot_2368.png').absolute()))
